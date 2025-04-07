88356e7c7388806c28c74c1468e092cbf73e9a5e
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-07 15:31:29 +0200

    Update Pwm.py

--- Pwm.py
+++ Pwm.py
@@ -5,17 +5,17 @@ led = PWMLED(12)
 
 def window():
     def update_led(value):
-        brightness = int(value) / 100  # Umwandlung von 0–100 zu 0.0–1.0
+        brightness = int(value) / 100  
         led.value = brightness
         print(f"Helligkeit: {brightness}")
 
     root = tk.Tk()
-    root.title("YT-DLP GUI")
+    root.title("Ygdrgdrfg")
     root.geometry('400x200')
     root.resizable(False, False)
 
     scale = tk.Scale(root, from_=0, to=100, orient='horizontal', length=300, command=update_led)
-    scale.set(0)  # Start bei 0 % Helligkeit
+    scale.set(0)
     scale.pack(pady=50)
 
     root.mainloop()
