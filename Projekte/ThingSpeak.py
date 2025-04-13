98459a96954e92a6687d458536fbbe1a2df9809d
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-13 20:48:44 +0200

    Update Projektstruktur

---
+++ Projekte/ThingSpeak.py
@@ -0,0 +1,11 @@
+﻿import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+pin = 4  # GPIO-Nummer, nicht Pin-Nummer
+
+humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+if humidity is not None and temperature is not None:
+    print(f'Temp: {temperature:.1f}°C  Luftfeuchtigkeit: {humidity:.1f}%')
+else:
+    print('Sensor nicht auslesbar')
