e93ef2df77f716270c5740c084b78953d2b3e5ea
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-14 13:34:09 +0200

    123213

--- Projekte/ThingSpeak.py
+++ Projekte/ThingSpeak.py
@@ -1,23 +1,37 @@
-﻿import time
+﻿# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
+# SPDX-License-Identifier: MIT
+
+import time
 import board
 import adafruit_dht
+from pulseio import PulseIn
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
 
-# DHT22 an GPIO17 (Pin 11)
-dht_device = adafruit_dht.DHT11(board.D4)
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single-board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
-try:
-    while True:
-        try:
-            temperature = dht_device.temperature
-            humidity = dht_device.humidity
-            print(f"Temp: {temperature:.1f}�C  Luftfeuchte: {humidity:.1f}%")
-        except RuntimeError as e:
-            # DHT22 liefert gelegentlich Lesefehler ? ignorieren und neu versuchen
-            print(f"Lesefehler: {e.args[0]}")
-        time.sleep(2)
+while True:
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
 
-except KeyboardInterrupt:
-    print("Messung beendet.")
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
 
-finally:
-    dht_device.exit()
+    time.sleep(2.0)
