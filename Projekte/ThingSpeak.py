74e00801ad109884b6e1b6096b8e2bc52f421a02
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-14 14:45:27 +0200

    hshshshsh

--- Projekte/ThingSpeak.py
+++ Projekte/ThingSpeak.py
@@ -1,37 +1,49 @@
-﻿# SPDX-FileCopyrightText: 2021 ladyada for Adafruit Industries
-# SPDX-License-Identifier: MIT
-
-import time
+﻿import time
+import os
 import board
+import json
 import adafruit_dht
-from pulseio import PulseIn
+import thingspeak
+from dotenv import load_dotenv
+
 # Initial the dht device, with data pin connected to:
-dhtDevice = adafruit_dht.DHT11(board.D4, use_pulseio=False)
+load_dotenv()
+dht_device = adafruit_dht.DHT22(board.D4, use_pulseio=False)
 
-# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
-# This may be necessary on a Linux single-board computer like the Raspberry Pi,
-# but it will not work in CircuitPython.
-# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
+WRITE_KEY = os.getenv("TS_W_KEY")
+READ_KEY = os.getenv("TS_R_KEY")
 
-while True:
+def measure(channel:thingspeak.Channel):
     try:
-        # Print the values to the serial port
-        temperature_c = dhtDevice.temperature
-        temperature_f = temperature_c * (9 / 5) + 32
-        humidity = dhtDevice.humidity
-        print(
-            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
-                temperature_f, temperature_c, humidity
-            )
-        )
+        temperature = dht_device.temperature
+        humidity = dht_device.humidity
+        channel.api_key = WRITE_KEY
+        params = {
+            "api_key" : "",
+            "field1" : temperature,
+            "field2" : humidity
+        }
+        channel.update(params)
+        channel.api_key = READ_KEY
+        field1_data = json.loads(channel.get_field_last(field=1))
+        field2_data = json.loads(channel.get_field_last(field=2))
+        field1 = field1_data["field1"]
+        field2 = field2_data["field2"]
+        print(f"Temperatur: {field1} humidity: {field2}")
 
-    except RuntimeError as error:
-        # Errors happen fairly often, DHT's are hard to read, just keep going
-        print(error.args[0])
-        time.sleep(2.0)
-        continue
-    except Exception as error:
-        dhtDevice.exit()
-        raise error
 
-    time.sleep(2.0)
+    except RuntimeError as e:
+        print(f"Sensorfehler: {e}")
+        
+    except KeyboardInterrupt:
+        dht_device.exit()
+
+
+if __name__ == "__main__":
+    channel = thingspeak.Channel(os.getenv("TS_CHANNEL"))
+    try:
+        while(True):
+            measure(channel)
+            time.sleep(5)
+    except KeyboardInterrupt:
+        dht_device.exit()
