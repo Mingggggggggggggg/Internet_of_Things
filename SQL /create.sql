4b1dc496d6572ae7e03c7c14fcc48ec39707d9a5
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-20 16:39:34 +0200

    213123

---
+++ SQL /create.sql
@@ -0,0 +1,8 @@
+﻿--CREATE TABLE dhtreadings(
+--id INTEGER PRIMARY KEY AUTOINCREMENT,
+--temperature NUMERIC,
+--humidity NUMERIC,
+--currentdate DATE,
+--currentime TIME);
+--CREATE TABLE sqlite_sequence(name,seq);
+
