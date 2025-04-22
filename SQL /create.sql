c5cec4fff12437ecbfad967d756c9690eba3a008
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-22 19:12:43 +0200

    Delete SQL  directory

--- SQL /create.sql
+++
@@ -1,8 +0,0 @@
-﻿--CREATE TABLE dhtreadings(
---id INTEGER PRIMARY KEY AUTOINCREMENT,
---temperature NUMERIC,
---humidity NUMERIC,
---currentdate DATE,
---currentime TIME);
---CREATE TABLE sqlite_sequence(name,seq);
-
