41d452dd2ef2ee02b90bc3c289c43fe3c4f9f69b
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-25 19:45:51 +0200

    123123

--- SQL Flask/SQL_Flask.py
+++ SQL Flask/SQL_Flask.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
     measureThread.start()
 
     try:
-        app.run(host='localhost', port=8181, debug=False)
+        app.run(host='localhost', port=8181, debug=False) #Flaskdebug modus aus, sonst doppelte messungen Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads Ich hasse Threads 
     except KeyboardInterrupt:
         GPIO.cleanup()
