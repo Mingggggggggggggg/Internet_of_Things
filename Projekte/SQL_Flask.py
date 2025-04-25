ea72b6fb110ec19e018b8da4d6e4b7a7820d226d
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-25 13:34:57 +0200

    12321321

---
+++ Projekte/SQL_Flask.py
@@ -0,0 +1,8 @@
+﻿from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
--- Projekte/SqlPy.py
+++ Projekte/SqlPy.py
@@ -7,5 +7,5 @@ cur = con.cursor()
 cur.execute("SELECT SQLITE_VERSION()")
 
 
-cur.execute("INSERT INTO dhtreadings VALUES(?, ?, ?, ?)", (2, 20, 50, "2025-04-25", "12:55:56"))
+cur.execute("INSERT INTO dhtreadings VALUES(?, ?, ?, ?, ?)", (2, 20, 50, "2025-04-25", "12:55:56"))
 con.commit()
