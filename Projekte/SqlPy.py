1fd11167a2dadb1a1257fb0f0450f3e50150a4e2
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-04-25 12:56:03 +0200

    132

---
+++ Projekte/SqlPy.py
@@ -0,0 +1,11 @@
+﻿import sqlite3
+
+
+con = sqlite3.connect("dht22.db")
+
+cur = con.cursor()
+cur.execute("SELECT SQLITE_VERSION()")
+
+
+cur.execute("INSERT INTO dhtreadings VALUES(?, ?, ?, ?)", (2, 20, 50, "2025-04-25", "12:55:56"))
+con.commit()
--- SQL/create.sql
+++ SQL/create.sql
@@ -1,1 +1,2 @@
-﻿
+﻿INSERT INTO dhtreadings(temperature, humidity, currentdate, currentime)
+    VALUES(22.4, 46, date("now"), time("now"));
