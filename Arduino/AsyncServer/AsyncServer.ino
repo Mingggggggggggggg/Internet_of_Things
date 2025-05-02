ad0e094eac8ea545c02cf324466801548a9fce0c
Ming <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-05-02 21:54:03 +0200

    123

---
+++ Arduino/AsyncServer/AsyncServer.ino
@@ -0,0 +1,165 @@
+#include <Arduino.h>
+#include "../keys.h"
+#ifdef ESP32
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#elif defined(ESP8266)
+#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#endif
+#include <ESPAsyncWebServer.h>
+#include "DHT.h"
+
+#define DHTPIN 4
+
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+
+AsyncWebServer server(80);
+const char* ssid = "WIFI_SSID";
+const char* password = "PASS";
+
+
+String readDHTTemperature() {
+  float t = dht.readTemperature();  // Standard: Werte in Celsius
+  // float t = dht.readTemperature(true); // Werte in Fahrenheit
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  } else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+String readDHTHumidity() {
+  float t = dht.readHumidity();  // Standard: Werte in Celsius
+  // float t = dht.readTemperature(true); // Werte in Fahrenheit
+  if (isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return "--";
+  } else {
+    Serial.println(t);
+    return String(t);
+  }
+}
+
+const char index_html[] PROGMEM = R"rawliteral(
+<!DOCTYPE HTML><html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet"
+  href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+  integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr"
+  crossorigin="anonymous">
+  <style>
+    html {
+      font-family: Arial;
+      display: inline-block;
+      margin: 0px auto;
+      text-align: center;
+    }
+    h2 { font-size: 3.0rem; }
+    p { font-size: 3.0rem; }
+    .units { font-size: 1.2rem; }
+    .dht-labels{
+      font-size: 1.5rem;
+      vertical-align:middle;
+      padding-bottom: 15px;
+    }
+  </style>
+</head>
+<body>
+  <h2>ESP32 DHT Server</h2>
+  <p>
+    <i class="fas fa-thermometer-half" style="color:#059e8a;"></i>
+    <span class="dht-labels">Temperature</span>
+    <span id="temperature">%TEMPERATURE%</span>
+    <sup class="units">&deg;C</sup>
+  </p>
+  <p>
+    <i class="fas fa-tint" style="color:#00add6;"></i>
+    <span class="dht-labels">Humidity</span>
+    <span id="humidity">%HUMIDITY%</span>
+    <sup class="units">&percnt;</sup>
+  </p>
+</body>
+<script>
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("temperature").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/temperature", true);
+  xhttp.send();
+}, 10000 ) ;
+setInterval(function ( ) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("humidity").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "/humidity", true);
+  xhttp.send();
+}, 10000 ) ;
+</script>
+</html>
+)rawliteral";
+
+String processor(const String& var) {
+  //Serial.println(var);
+  if (var == "TEMPERATURE") {
+    return readDHTTemperature();
+  } else if (var == "HUMIDITY") {
+    return readDHTHumidity();
+  }
+  return String();
+}
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("WiFi Failed!\n");
+    return;
+  }
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/html", index_html, processor);
+  });
+  server.on("/temperature", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/plain", readDHTTemperature().c_str());
+  });
+  server.on("/humidity", HTTP_GET, [](AsyncWebServerRequest *request) {
+    request->send_P(200, "text/plain", readDHTHumidity().c_str());
+  });
+  server.onNotFound(notFound);
+  server.begin();
+  dht.begin();
+}
+
+void loop() {
+/*  delay(5000);
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Fehler beim Auslesen des Sensors");
+    return;
+  }
+  Serial.println("______________________________________________________");
+  Serial.println("Luftfeuchtigkeit: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperatur: ");
+  Serial.print(t);
+  Serial.println(" \xe2\x84\x83"); // UTF Code for Grad Celsius
+  Serial.println("______________________________________________________");
+  Serial.println("  ");*/
+}
