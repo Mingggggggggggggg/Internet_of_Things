b7117c0383f606f63d5093fe36cd50d8f268c26a
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-05-11 17:58:39 +0200

    123

---
+++ Projekte/SqlPy.py
@@ -0,0 +1,11 @@
+﻿import sqlite3
+
+
+con = sqlite3.connect("dht22.db")
+
+cur = con.cursor()
+cur.execute("SELECT SQLITE_VERSION()")
+
+
+cur.execute("INSERT INTO dhtreadings VALUES(?, ?, ?, ?)", (1, 20, 50, "25.01.2010", "13:23"))
+con.commit()
