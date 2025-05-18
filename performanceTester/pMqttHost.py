81ecbeaa136f11a4aef98dec47ad853f1523e2f4
Mingggggggggggggg <98337759+Mingggggggggggggg@users.noreply.github.com>
2025-05-18 11:08:53 +0200

    10hz 1kb Data LAN

--- performanceTester/pMqttHost.py
+++ performanceTester/pMqttHost.py
@@ -14,7 +14,7 @@ MQTT_SUB_LATMESSAGE = "/esp32/latencyMessage"
 totalSend = 100
 qos = 0
 sleep = 0.1
-messageSizeReal = 0 #1024 für 1kb
+messageSizeReal = 1024 #1024 für 1kb
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
